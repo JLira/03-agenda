dc77dcd81feffe2685f1d42e3c4a447eb4be96c1
jlira <jobson.lira@gmail.com>
2023-12-27 21:40:48 -0300

    chore: adding faker

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,12 @@
 from django.utils import timezone
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -20,6 +24,10 @@ class Contact(models.Model):
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True)
+
+
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
