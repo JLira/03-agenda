c314b589ea3099e3ded85b876cf248e7caa9a037
jlira <jobson.lira@gmail.com>
2023-12-28 21:00:38 -0300

    chore: implementing pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,18 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, render, redirect
 from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('first_name')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('first_name')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -24,7 +29,8 @@ def search(request):
     if search_value == '':
         return redirect('contact:index')
 
-    contacts = Contact.objects.filter(show=True)\
+    contacts = Contact.objects \
+        .filter(show=True)\
         .filter(
         Q(first_name__icontains=search_value) |
         Q(last_name__icontains=search_value) |
@@ -32,12 +38,17 @@ def search(request):
         Q(email__icontains=search_value) 
     )\
         .order_by('first_name')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     # print(contacts.query)
 
     context = {
-        'contacts': contacts,
-        'site_title': 'Search - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
@@ -52,7 +63,7 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
-    site_title = f'{ single_contact.first_name} {single_contact.last_name}'
+    site_title = f'{ single_contact.first_name} {single_contact.last_name} -'
 
     context = {
         'contact': single_contact,
